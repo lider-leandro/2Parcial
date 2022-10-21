6d5ae7e97ac73964fe84e89174737d767965f692
lider-leandro <liderlobo7@gmail.com>
2022-10-21 12:07:57 -0400

    tercercomponente

--- src/components/TercerComponente.jsx
+++ src/components/TercerComponente.jsx
@@ -1,3 +1,4 @@
+export {SegundoComponente};
 import React from "react";
 import {ProductRow} from "./ProductRow";
 import "../styles/productCategory.css";
@@ -5,7 +6,8 @@ import "../styles/productCategory.css";
 function TercerComponente() {
     return ( 
     <div className="TercerComponente"> 
-       <h3>diana</h3>
+       <button>Follow</button>
+        <button>telegram</button>
     </div>
     );
 }
